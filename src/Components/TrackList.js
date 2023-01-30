5a29a980caa70244e22db79ec366880e32664360
SchinoTheRapper <110900056+Moschifire@users.noreply.github.com>
2023-01-30 20:57:57 +0100

    this is commitment

--- src/Components/TrackList.js
+++ src/Components/TrackList.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { BsVolumeUpFill, BsMusicNoteList } from "react-icons/bs"
+import { FaDesktop } from "react-icons/fa"
 
 function TrackList() {
   return (
     <div className='trackList'>
-        
+        <div className="top">
+            <img src="" alt="" />
+            <p>
+                Sample Name <span>Artiste</span>
+            </p>
+        </div>
+        <div className="bottom">
+            <i><BsVolumeUpFill /></i>
+            <input type="range" />
+            <i><BsMusicNoteList /></i>
+            <i><FaDesktop /></i>
+        </div>
     </div>
   )
 }
