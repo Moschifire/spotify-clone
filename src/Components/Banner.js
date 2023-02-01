e7fd1b896f670cacaee30bbbdf2c910cd16fa695
SchinoTheRapper <110900056+Moschifire@users.noreply.github.com>
2023-02-01 19:36:22 +0100

    banner completed

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,11 +1,33 @@
 import React from 'react'
 import Artist from "../img/artist.jpg";
 import Check from "../img/check.png";
+import { FaEllipsisH, FaHeadphones, FaCheck } from "react-icons/fa"
 
 function Banner() {
   return (
     <div className='banner'>
-        <img src="" alt="" />
+        <img src={Artist} alt="" className='bannerImg' />
+
+        <div className="content">
+            <div className="breadCrumb">
+                <p>Home <span>/Popular Artiste</span></p>
+                <i><FaEllipsisH /></i>
+            </div>
+            <div className="artist">
+                <div className="left">
+                    <div className="name">
+                        <h2>A-Ha</h2>
+                        <img src={Check} alt="" />
+                    </div>
+                    <p><i><FaHeadphones /></i> 5,000,230 <span>Monthly Listeners</span></p>
+                </div>
+                <div className="right">
+                    <a href="#">Play</a>
+                    <a href="#"><i><FaCheck /></i> Following</a>
+                </div>
+            </div>
+        </div>
+        <div className="bottomLayer"></div>
     </div>
   )
 }
